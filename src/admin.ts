80051f6f3b76c1b84339fe03236154025ea07016
Núria Ramoneda <nuriarai@gmail.com>
2023-05-14 17:19:57 +0200

    Add tinyeditor to form & submit button

--- src/admin.ts
+++ src/admin.ts
@@ -2,3 +2,4 @@ import "./assets/scss/admin.scss";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
 import "@fontsource/saira-extra-condensed/600.css";
+import "tiny-editor";
